e2f61ec8f922b0452085c2e40b82ea17738fdc57
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-09-28 18:43:08 -0400

    fix execution script

--- src/code_executioner.py
+++ src/code_executioner.py
@@ -1,11 +1,12 @@
 import subprocess
 
 def execute_python_code(python_code_as_string: str):
-    python_code_as_string = "import" + python_code_as_string \
+    python_code_as_string = python_code_as_string \
         .replace("```python", "") \
         .split("```")[0] \
-        .split("import")[1]
+        .split("import")[1:]
     
     with open('executable.py', 'w') as f:
-        f.write(python_code_as_string)
-    subprocess.call("python3.11 ./executable.py", shell=True)
+        for python_code_line in python_code_as_string[1:]:
+            f.write("import"+python_code_line)
+    subprocess.call("python3 ./executable.py", shell=True)
