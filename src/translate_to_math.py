f69e0f58ab0a5296fbb4bb7933286eaa61262779
David Zollikofer <david@zollikofer.co>
2024-09-28 15:11:48 -0400

    added basic structure

---
+++ src/translate_to_math.py
@@ -0,0 +1,17 @@
+# from ai21 import AI21Client
+
+# messages = [ChatMessage(content="Who was the first emperor of rome", role="user")]
+
+# client = AI21Client()
+
+# response = client.chat.completions.create(
+#   messages=messages,
+#   model="jamba-1.5-mini",
+#   stream=True
+# )
+
+# for chunk in response:
+#   print(chunk.choices[0].delta.content, end="")
+
+def translate_to_math(input):
+    return input
